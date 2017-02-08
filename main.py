bf826407aee13e9d06f25d679975e34fc3a2a4ac
Jaehyun Lim <lim0606@gmail.com>
2017-02-08 14:29:11 +0900

    add mnist example

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ parser.add_argument('--workers', type=int, help='number of data loading workers'
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--loadSize',  type=int, default=64, help='the height / width of the input image (it will be croppred)')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
+parser.add_argument('--nc', type=int, default=3,   help='number of channels in input (image)')
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
@@ -86,6 +87,13 @@ elif opt.dataset == 'cifar10':
                                transforms.ToTensor(),
                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                            ]))
+elif opt.dataset == 'mnist':
+    dataset = dset.MNIST(root=opt.dataroot, download=True,
+                         transform=transforms.Compose([
+                             transforms.Scale(opt.imageSize),
+                             transforms.ToTensor(),
+                             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                         ]))
 assert dataset
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
                                          shuffle=True, num_workers=int(opt.workers))
@@ -94,7 +102,7 @@ ngpu = int(opt.ngpu)
 nz = int(opt.nz)
 ngf = int(opt.ngf)
 ndf = int(opt.ndf)
-nc = 3
+nc = opt.nc 
 n_extra_layers = int(opt.n_extra_layers)
 
 # custom weights initialization called on netG and netD
--- main.sh
+++ main.sh
@@ -5,6 +5,16 @@ filename=$(date +"%Y%m%d-%H%M%S-%N")".log"
 
 mkdir logs
 
+# mnist with DCGAN 
+export CUDA_VISIBLE_DEVICES=1
+python main.py --cuda --dataset mnist --dataroot /media/data0/image/mnist --imageSize 32 --nc 1 --niter 5000 --experiment samples/mnist_dcgan #2>&1 | tee logs/mnist_dcgan_$filename
+
+
+# celeba with DCGAN 
+export CUDA_VISIBLE_DEVICES=1
+python main.py --cuda --dataset folder --dataroot /media/data0/image/celeba --loadSize 96 --niter 5000 --experiment samples/celeba_dcgan 2>&1 | tee logs/celeba_dcgan_$filename
+
+
 # cifar10 with DCGAN 
 export CUDA_VISIBLE_DEVICES=0
 python main.py --cuda --dataset cifar10 --dataroot /media/data0/image/cifar10 --niter 5000 --experiment samples/cifar10_dcgan 2>&1 | tee logs/cifar10_dcgan_$filename
@@ -13,10 +23,7 @@ python main.py --cuda --dataset cifar10 --dataroot /media/data0/image/cifar10 --
 export CUDA_VISIBLE_DEVICES=1
 python main.py --cuda --mlp_G --ngf 512 --dataset cifar10 --dataroot /media/data0/image/cifar10 --niter 5000 --experiment samples/cifar10_mlp 2>&1 | tee logs/cifar10_mlp_$filename
 
+
 # lsun with DCGAN
 export CUDA_VISIBLE_DEVICES=0
 python main.py --cuda --dataset lsun --dataroot /media/data0/image/lsun --experiment samples/lsun_dcgan 2>&1 | tee logs/lsun_dcgan_$filename
-
-# celeba with DCGAN 
-export CUDA_VISIBLE_DEVICES=1
-python main.py --cuda --dataset folder --dataroot /media/data0/image/celeba --loadSize 96 --niter 5000 --experiment samples/celeba_dcgan 2>&1 | tee logs/celeba_dcgan_$filename
